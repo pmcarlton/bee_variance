f857f801cb0dd11ce6b550c02062b77c932cdca8
Peter Carlton <pmcarlton@mac.com>
2018-06-11 10:18:22 +0900

    Create bee_image_extract.sh

---
+++ bee_image_extract.sh
@@ -0,0 +1,15 @@
+# Extract the target images from the supplemental data PDF file and equalize the image sizes:
+
+```{bash} 
+pdfimages -j $HOME/aar4975_Howard_SM.pdf $HOME/imgdir/bees
+cd imgdir
+mkdir targets
+for i in $(seq -w 13 4 396 );
+	do mv bees-$i targets/;
+	done
+mv bees-010.jpg targets/
+cd targets
+for i in bees*jpg;
+	do convert -resize 113x113\! $i rescaled$i;
+	done # make all 113x113, the most common size
+```
